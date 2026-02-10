e37a92a9f544a9c75363158a909cef8c38cbf829
LEHM0 <kennykawaii00@gmail.com>
2026-02-10 15:27:34 -0600

    Working on Door Obstacle Prefab

--- ChronoQuest/Assets/Scripts/Game Logic/DoorController.cs
+++ ChronoQuest/Assets/Scripts/Game Logic/DoorController.cs
@@ -1,14 +1,26 @@
+using System.Collections;
 using UnityEngine;
 
 public class DoorController : Obstacle
 {
     public GameObject doorModel;
 
+    private Vector3 startPos = Vector3.zero;
+    private Vector3 endPos = Vector3.up * 5;
+    private float time = 5.0f;
+
     public override void ClearObstacle()
     {
         //Removal logic
-        doorModel.transform.Translate(Vector3.up * Time.deltaTime * 200); //Fix: Door moves instantly, not over time
+        StartCoroutine(MoveUp());
         cleared = true;
         Debug.Log($"{obstacleID} removed!");
+    }
+
+    IEnumerator MoveUp()
+    {
+        doorModel.transform.position += Vector3.Lerp(startPos, endPos, time);
+        //Fix: Door moves instantly, not over time - Working On
+        yield return null;
     }
 }
