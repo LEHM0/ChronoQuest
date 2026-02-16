2cc23ff6166ad1ef475d5cc3cf49ee843843fbed
Luis Lima <160065161+ll1111472@users.noreply.github.com>
2026-02-16 15:36:29 -0600

    new
    new

---
+++ ChronoQuest/Assets/Scripts/Game Logic/SkyManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyManager : MonoBehaviour
+{
+
+    public float skySpeed;
+
+    private void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * skySpeed);
+    }
+}
